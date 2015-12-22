3ea5345e7a143ab38f38f9a775cee7c2c6459892
Bastiaan Quast <bquast@gmail.com>
2015-12-22 11:32:09 +0100

    rename back, sort order

--- R/scan.R
+++ R/scan.R
@@ -1,12 +1,12 @@
-#' scan
+#' explore
 #' 
 #' @export
 
 
-scan <- function(data) {
+explore <- function(data) {
   chr <- sapply(data,  attr, "label")
-  chr <- paste('#', chr)
   ids <- names(chr)
   names(chr) <- NULL
+  chr <- paste('#', chr)
   data.frame(id = ids, description = chr)
 }
